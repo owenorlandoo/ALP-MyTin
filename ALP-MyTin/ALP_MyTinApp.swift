4e7a9c03ab4c4672af519108a53664721bfa9034
Owen Orlando <oorlando@student.ciputra.ac.id>
2024-05-30 12:53:34 +0700

    Initial Commit

---
+++ ALP-MyTin/ALP_MyTinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MyTinApp.swift
+//  ALP-MyTin
+//
+//  Created by MacBook Pro on 30/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ALP_MyTinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
